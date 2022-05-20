08696eff27c2a3b688689c23efb1c9293896dc30
Adam Johnson <vaeb@hey.com>
2022-05-20 03:06:19 +0100

    Delete src directory

--- src/Simple.java
+++
@@ -1,9 +0,0 @@
-package my.petty.examples;
-
-public class Simple {
-    public static void main(String[] args) {
-        String s = new String("Abc");
-        String u = "bc";
-        assert(!s.contains(u)); // This assert failure should be found by JBMC!
-    }                           
-}
